06b5d138741da8acc228291df0e37eba014c608e
LauraPastor <laurapastorj@gmail.com>
2023-12-22 13:16:42 +0100

    Add todo value

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue'
 </script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
 
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:wght@200;300;400;500;600;700;800&family=Rubik:wght@300;400;500;700&display=swap');
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Rubik', sans-serif;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
 </style>
