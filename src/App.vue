d71d595514b770029d4253063f171f2db358f29e
LauraPastor <laurapastorj@gmail.com>
2023-12-22 11:56:32 +0100

    Add skeleton for Vue todos

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:wght@200;300;400;500;600;700;800&family=Rubik:wght@300;400;500;700&display=swap');
+
+</style>
