784f034c3bccf9b8e6b1e275cfc3942508fac883
LauraPastor <laurapastorj@gmail.com>
2023-12-23 14:41:16 +0100

    Add edit functionality to the todo list

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -11,20 +11,22 @@ const props = defineProps({
         required: true,
     },
 });
-defineEmits(["toggle-complete"]);
+defineEmits(["toggle-complete", "edit-todo", "update-todos"]);
 </script>
 <template>
     <li>
         <input type="checkbox" :checked="todo.isCompleted" @input="$emit('toggle-complete', index)" />
         <div class="todo">
-            <input v-if="todo.isEditing" type="text" :value="todo.todo" />
+            <input v-if="todo.isEditing" type="text" :value="todo.todo"
+                @input="$emit('update-todo', $event.target.value, index)" />
             <span v-else :class="{ 'completed-todo': todo.isCompleted }">
                 {{ todo.todo }}
             </span>
         </div>
         <div class="todo-actions">
-            <Icon v-if="todo.isEditing" icon="ph:check-circle" color="#41b080" width="22" />
-            <Icon v-else icon="ph:pencil-fill" color="#41b080" width="22" />
+            <Icon v-if="todo.isEditing" icon="ph:check-circle" color="#41b080" width="22"
+                @click="$emit('edit-todo', index)" />
+            <Icon v-else icon="ph:pencil-fill" color="#41b080" width="22" @click="$emit('edit-todo', index)" />
             <Icon icon="ph:trash" color="#f95e5e" width="22" />
         </div>
     </li>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -17,6 +17,12 @@ const createTodo = (todo) => {
 const toggleTodoComplete = (todoPos) => {
   todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
 };
+const toggleEditTodo = (todoPos) => {
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+};
+const updateTodo = (todoVal, todoPos) => {
+  todoList.value[todoPos].todo = todoVal;
+};
 </script>
 
 <template>
@@ -24,7 +30,8 @@ const toggleTodoComplete = (todoPos) => {
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
     <ul class="todo-list" v-if="todoList.length > 0">
-      <TodoItem v-for="(todo, index) in todoList" :todo="todo" :index="index" @toggle-complete="toggleTodoComplete" />
+      <TodoItem v-for="(todo, index) in todoList" :todo="todo" :index="index" @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo" @update-todo="updateTodo" />
     </ul>
     <p class="todos-msg" v-else>
       <Icon icon="noto-v1:relieved-face" width="22" />
